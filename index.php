c53dc612d9bcd8777d6c7b01192b7ffed7d7082b
Robert Marczak <robert.m.1305@gmail.com>
2018-08-01 12:53:22 +0100

    Required FileCarRepository.php

--- index.php
+++ index.php
@@ -5,4 +5,15 @@
 */
 // require 'src/Class_A_B_example.php';
 
-require 'src/video_player_example.php';
+
+/*
+*Uncomment to see this example
+*/
+// require 'src/video_player_example.php';
+
+
+/*
+*Uncomment to see this example
+*/
+
+require 'src/repository_example.php';
