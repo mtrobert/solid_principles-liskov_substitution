d0eb14e14d63a2ee2f5124fa4e6499d4359257f0
Robert Marczak <robert.m.1305@gmail.com>
2018-08-01 12:00:52 +0100

    Created example function, video file and the video player class

--- src/video_player_example.php
+++ src/video_player_example.php
@@ -2,3 +2,14 @@
 
 
  use App\src\{AviVideoPlayer, VideoPlayer};
+
+function play($obj,$videoFile){
+  $obj->play($videoFile);
+}
+$videoFile = 'ShawshankRedemption.avi';
+
+// $videoPlayer = new VideoPlayer(); //commented out for demo purposes only
+$videoPlayer = new AviVideoPlayer();
+
+
+play($videoPlayer,$videoFile);
