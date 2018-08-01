cdcac2f7df8dd64da9fd0d11d4e03602a40ce1b2
Robert Marczak <robert.m.1305@gmail.com>
2018-08-01 13:02:44 +0100

    Created DbCarRepository.php

---
+++ src/DbCarRepository.php
@@ -0,0 +1,12 @@
+<?php namespace App\src;
+
+
+class DbCarRepository implements CarRepositoryInterface
+{
+  public function getAll()
+  {
+    // return through db
+    $CarRepo = new stdClas();
+    return $CarRepo;
+  }
+}
