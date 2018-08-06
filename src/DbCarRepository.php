5a1904c2f4a0bec4b811d66c5066ae8fc3b44595
Robert Marczak <robert.m.1305@gmail.com>
2018-08-06 11:48:15 +0100

    Added comment to the carRepo var. Declared 'use' for to stdClass

--- src/DbCarRepository.php
+++ src/DbCarRepository.php
@@ -1,12 +1,12 @@
 <?php namespace App\src;
 
-
+use \stdClass;
 class DbCarRepository implements CarRepositoryInterface
 {
   public function getAll()
   {
     // return through db
-    $CarRepo = new stdClas();
+    $CarRepo = new stdClass(); //represents data from db
     return $CarRepo;
   }
 }
